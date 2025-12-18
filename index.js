61af3b68ac62f9d5144ab78f8c1699ff71998858
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-12-18 22:18:49 +0100

    Add IDs to info boxes for dynamic content updates

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Happy developing ✨')
+const converterInput = document.getElementById("number-input")
+const converterButton = document.getElementById("number-btn")
+
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
