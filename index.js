64727d1efda2306ed36fa452d2a92991a3280c34
Piotr Jelonek <piotrek.jelonek@icloud.com>
2025-12-19 12:58:21 +0100

    Add conversion functions for length, volume, and mass

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ const massEl = document.getElementById("mass-el")
 converterButton.addEventListener("click", function () {
     console.log("button clicked!")
 })
+
+function lengthConvert(number) {
+    let metersToFeet = number * 3.28084
+    let feetToMeters = number * 0.3048
+
+    return [metersToFeet.toFixed(3), feetToMeters.toFixed(3)]
+}
+
+function volumeConvert(number) {
+    let litersToGallons = number / 3.785411784
+    let gallonsToLiters = number * 3.785411784
+
+    return [litersToGallons.toFixed(3), gallonsToLiters.toFixed(3)]
+}
+
+function massConvert(number) {
+    let kilogramsToPounds = number * 2.2046226218487757
+    let poundsToKilograms = number * 0.45359237
+
+    return [kilogramsToPounds.toFixed(3), poundsToKilograms.toFixed(3)]
+}
+
